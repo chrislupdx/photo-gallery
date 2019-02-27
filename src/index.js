72c79135307c7a73456d9cacba38b89cb0b4bb5b
dreamer <wildetangeant@gmail.com>
2019-02-27 09:58:19 -0800

    tests passed!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import renderedPhoto from './renderedPhoto.js';
-import images from './data/images.js';
+import images from '../data/images.js';
 
 //grab photogal ul node
 const ulFill = document.getElementById('photogal');
-
+console.log(ulFill);
 //use forEach to go through the images, render to UL as Li components
 images.forEach(function(image) {
     const dom = renderedPhoto(image);
